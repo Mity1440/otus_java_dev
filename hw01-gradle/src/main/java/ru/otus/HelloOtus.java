be9b14c0e94f8c87b091fff213b545d20c7046c8
mitia.mackarevich@yandex.ru <mitia.mackarevich@yandex.ru>
2021-10-07 10:12:42 +0300

    feat: Добавлено использование объекта из библиотеки guava

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -1,7 +1,24 @@
 package ru.otus;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
 public class HelloOtus{
     public static void main(String[]args){
-        
+
+        System.out.println("Hello OTUS");
+
+        BiMap daysOfWeek = HashBiMap.create();
+
+        daysOfWeek.put(1, "Monday");
+        daysOfWeek.put(2, "Tuesday");
+        daysOfWeek.put(3, "Wednesday");
+        daysOfWeek.put(4, "Thursday");
+        daysOfWeek.put(5, "Friday");
+        daysOfWeek.put(6, "Saturday");
+        daysOfWeek.put(7, "Sunday");
+
+        System.out.println(daysOfWeek);
+
     }
 }
