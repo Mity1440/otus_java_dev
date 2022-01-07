1f93cb4ca362225a947f242c6ac6331c7ef538d5
Дмитрий Макаревич <36016849+Mity1440@users.noreply.github.com>
2022-01-07 18:32:51 +0200

    feat: hw15-patterns (#8)

---
+++ hw15-structuralPatterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -0,0 +1,33 @@
+package ru.otus.listener.homework;
+
+import ru.otus.listener.Listener;
+import ru.otus.model.Message;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class HistoryListener implements Listener, HistoryReader {
+
+    private final Map<Long, Message> previousStates;
+
+    public HistoryListener() {
+        this.previousStates = new HashMap<>();
+    }
+
+    @Override
+    public void onUpdated(Message msg) {
+
+        try {
+            previousStates.put(msg.getId(), msg.clone());
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException();
+        }
+
+    }
+
+    @Override
+    public Optional<Message> findMessageById(long id) {
+        return Optional.ofNullable(previousStates.get(id));
+    }
+}
