baba3e7986f8df1d0f76feb6a35ba7419ee14fe6
d.makarevich@sodru.com <d.makarevich@sodru.com>
2022-03-21 23:29:00 +0300

    fix: Исправление замечений

--- hw33-multiprocess/src/main/java/ru/otus/Client.java
+++ hw33-multiprocess/src/main/java/ru/otus/Client.java
@@ -32,7 +32,7 @@ public class Client {
 
                             @Override
                             public void onNext(Digit um) {
-                                counter.incrementAndGet();
+                                counter.set(um.getValue());
                             }
 
                             @Override
